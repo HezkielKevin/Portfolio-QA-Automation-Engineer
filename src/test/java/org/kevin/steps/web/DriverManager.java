29fb955cd33276cab4615528243f1da9faf913df
HezkielKevin <90523729+HezkielKevin@users.noreply.github.com>
2026-06-27 09:43:49 +0700

    Add AfterStep hook to attach screenshot to Cucumber report for Web tests

--- src/test/java/org/kevin/steps/web/DriverManager.java
+++ src/test/java/org/kevin/steps/web/DriverManager.java
@@ -2,12 +2,28 @@ package org.kevin.steps.web;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverManager {
     private static WebDriver driver;
+
+    @AfterStep("@web")
+    public void takeScreenshotAfterStep(Scenario scenario) {
+        if (driver != null) {
+            try {
+                final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+                scenario.attach(screenshot, "image/png", "Screenshot - " + scenario.getName());
+            } catch (Exception e) {
+                System.out.println("Gagal mengambil screenshot: " + e.getMessage());
+            }
+        }
+    }
 
     @Before("@web")
     public void setUp() {
